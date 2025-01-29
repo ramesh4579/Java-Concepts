d8960856c42a13a87c8566ca1f34632c9481a5d2
ramesh4579 <meshna.19.2019@gmail.com>
2025-01-29 14:02:06 -0800

    Update AppMain.java

--- singleton/AppMain.java
+++ singleton/AppMain.java
@@ -3,5 +3,6 @@ package singleton;
 public class AppMain {
     public static void main(String[] args) {
         Singleton.INSTANCE.print();
+        SingletonClass singletonClass = SingletonClass.getInstance();
     }
 }
